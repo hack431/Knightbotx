9d09905ce0c87487823ac2a3d6031e2da80ed396
hack431 <undefeatable126@gmail.com>
2025-06-21 20:33:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'DJJOHN',
+  packname: 'KNIGHT BOT',
   author: '‎',
-  botName: "FELIX",
-  botOwner: 'hack431', // Your name
+  botName: "KNIGHT BOT",
+  botOwner: 'DJJOHN', // Your name
   ownerNumber: '255677510751', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.6",
 };
